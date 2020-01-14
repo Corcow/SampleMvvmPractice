9e4ad3299e56dd6713a06e2b2fe358a5756f0ba5
Hyunwoo <cho7673@hansung.ac.kr>
2020-01-15 00:07:35 +0900

    Fragment 공유 예제 추가.

--- app/src/main/java/com/corcow/samplemvvmproject/main/MainActivity.kt
+++ app/src/main/java/com/corcow/samplemvvmproject/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.corcow.samplemvvmproject.view.main
+package com.corcow.samplemvvmproject.main
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -6,11 +6,17 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.corcow.samplemvvmproject.R
 import com.corcow.samplemvvmproject.databinding.ActivityMainBinding
-import com.corcow.samplemvvmproject.view.practice.Practice1Activity
-import com.corcow.samplemvvmproject.view.practice.Practice2Activity
-import com.corcow.samplemvvmproject.view.practice.Practice3Activity
+import com.corcow.samplemvvmproject.practice.Practice1Activity
+import com.corcow.samplemvvmproject.practice.Practice2Activity
+import com.corcow.samplemvvmproject.practice.Practice3Activity
+import com.corcow.samplemvvmproject.vmshare.VmShareActivity
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Android architecture component 스터디
+     * 
+     */
 
     val mBinding: ActivityMainBinding by lazy {
         DataBindingUtil.setContentView(this, R.layout.activity_main) as ActivityMainBinding
@@ -29,6 +35,10 @@ class MainActivity : AppCompatActivity() {
 
         mBinding.btnPractice3.setOnClickListener {
             startActivity(Intent(this@MainActivity, Practice3Activity::class.java))
+        }
+
+        mBinding.btnVmShare.setOnClickListener {
+            startActivity(Intent(this@MainActivity, VmShareActivity::class.java))
         }
 
     }
